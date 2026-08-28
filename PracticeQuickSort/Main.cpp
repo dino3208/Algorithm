4928d044175eaa89494c587ed2a93b8f82dfa3c7
dino3208 <dino3208@naver.com>
2026-08-28 17:44:41 +0900

    병합정렬 1번 예제 완료

--- PracticeQuickSort/Main.cpp
+++ PracticeQuickSort/Main.cpp
@@ -36,5 +36,85 @@ private:
 
 int Partition(Person** array, int left, int right)
 {
+	int pivot = array[left]->GetAge();
+	int low = left + 1;
+	int high = right;
 
+	while (low <= high)
+	{
+		// 피벗 나이보다 큰 객체를 찾는다
+		while (low <= right && array[low]->GetAge() <= pivot)
+		{
+			++low;
+		}
+
+		// 피벗 나이보다 작은 객체를 찾는다
+		while (high > left && array[high]->GetAge() >= pivot)
+		{
+			--high;
+		}
+
+		if (low > high)
+		{
+			break;
+		}
+
+		Swap(array[low], array[high]);
+	}
+
+	Swap(array[left], array[high]);
+	return high;
+}
+
+void QuickSort(Person* array[], int left, int right)
+{
+	if (left < right)
+	{
+		int pivotIndex = Partition(array, left, right);
+
+		QuickSort(array, left, pivotIndex - 1);
+		QuickSort(array, pivotIndex + 1, right);
+	}
+}
+
+void PrintArray(Person* array[], int length)
+{
+	for (int ix = 0; ix < length; ++ix)
+	{
+		std::cout << "이름: " << array[ix]->GetName()
+			<< "나이: " << array[ix]->GetAge() << "\n";
+	}
+
+	std::cout << "\n";
+}
+
+int main()
+{
+	Person* people[] =
+	{
+		new Person("Alice", 25),
+		new Person("Mother", 70),
+		new Person("Bob", 30),
+		new Person("Father", 72),
+		new Person("Charlie", 20),
+		new Person("Ronnie", 40)
+	};
+	
+	int length = sizeof(people) / sizeof(people[0]);
+
+	std::cout << "====== 퀵 정렬 전 배열 ======\n";
+	PrintArray(people, length);
+
+	QuickSort(people, 0, length - 1);
+
+	std::cout << "====== 퀵 정렬 후 배열 ======\n";
+	PrintArray(people, length);
+
+	for (int ix = 0; ix < length; ++ix)
+	{
+		delete people[ix];
+	}
+
+	std::cin.get();
+	return 0;
 }
