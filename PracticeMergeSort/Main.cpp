2e84c130445f53d55b30082c52613c74527df2a2
dino3208 <dino3208@naver.com>
2026-08-30 11:07:05 +0900

    병합정렬 예제 완료

--- PracticeMergeSort/Main.cpp
+++ PracticeMergeSort/Main.cpp
@@ -1,11 +1,33 @@
-#include <cstring>
 #include <iostream>
 
+class Person
+{
+public:
+	Person(const char* name, int age)
+		:age(age)
+	{
+		int length = static_cast<int>(strlen(name) + 1);
+		this->name = new char[length];
+		strcpy_s(this->name, length, name);
+	}
+
+	~Person()
+	{
+		delete[] name;
+	}
+
+	const char* GetName() const { return name; }
+	int GetAge() const { return age; }
+
+private:
+	char* name;
+	int age;
+};
+
 void Merge(
-	const char* array[],
-	const char* leftArray[], int leftArrayLength,
-	const char* rightArray[], int rightArrayLength
-)
+	Person* array[],
+	Person* leftArray[], int leftArrayLength,
+	Person* rightArray[], int rightArrayLength)
 {
 	int leftIndex = 0;
 	int rightIndex = 0;
@@ -13,7 +35,7 @@ void Merge(
 
 	while (leftIndex < leftArrayLength && rightIndex < rightArrayLength)
 	{
-		if (strcmp(leftArray[leftIndex], rightArray[rightIndex]) <= 0)
+		if (leftArray[leftIndex]->GetAge() <= rightArray[rightIndex]->GetAge())
 		{
 			array[mergedIndex] = leftArray[leftIndex];
 			++leftIndex;
@@ -23,11 +45,26 @@ void Merge(
 			array[mergedIndex] = rightArray[rightIndex];
 			++rightIndex;
 		}
+
+		++mergedIndex;
+	}
+
+	// 잔여 원소 처리
+	while (leftIndex < leftArrayLength)
+	{
+		array[mergedIndex] = leftArray[leftIndex];
+		++leftIndex;
+		++mergedIndex;
+	}
+	while (rightIndex < rightArrayLength)
+	{
+		array[mergedIndex] = rightArray[rightIndex];
+		++rightIndex;
 		++mergedIndex;
 	}
 }
 
-void MergeSort(const char* array[], int length)
+void MergeSort(Person* array[], int length)
 {
 	if (length <= 1)
 	{
@@ -38,11 +75,11 @@ void MergeSort(const char* array[], int length)
 	int leftArrayLength = middle;
 	int rightArrayLength = length - middle;
 
-	const char** leftArray = new const char* [leftArrayLength];
-	const char** rightArray = new const char* [leftArrayLength];
+	Person** leftArray = new Person * [leftArrayLength];
+	Person** rightArray = new Person * [rightArrayLength];
 
-	memcpy(leftArray, array, sizeof(int) * leftArrayLength);
-	memcpy(rightArray, array + middle, sizeof(int) * rightArrayLength);
+	memcpy(leftArray, array, sizeof(Person*) * leftArrayLength);
+	memcpy(rightArray, array, sizeof(Person*) * rightArrayLength);
 
 	MergeSort(leftArray, leftArrayLength);
 	MergeSort(rightArray, rightArrayLength);
@@ -53,5 +90,4 @@ void MergeSort(const char* array[], int length)
 
 	delete[] leftArray;
 	delete[] rightArray;
-
 }
